912eb18289daf8b0725bcc8012e986317c747946
FernandaRojas152 <fernandarojas152@hotmail.com>
2023-01-27 15:32:33 -0500

    adding mock heroes to test angular ngFor directive

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero";
+
+export const HEROES:  Hero[] =[
+    {id: 12, name: 'Doctor Strange'},
+    {id: 22, name: 'All Might'},
+    {id: 13, name: 'Flash'},
+    {id: 14, name: 'Scarlet Witch'},
+    {id: 15, name: 'Hulk'},
+    {id: 16, name: 'Captain America'},
+];
