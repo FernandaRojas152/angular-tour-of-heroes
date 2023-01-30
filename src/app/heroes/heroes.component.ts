6499373e5a1629ed747822db088f5b6697fd315b
FernandaRojas152 <fernandarojas152@hotmail.com>
2023-01-30 13:03:16 -0500

    router added and changing css

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  constructor(private heroService: HeroService){}
+export class HeroesComponent implements OnInit{
+  constructor(private heroService: HeroService, private messageService: MessageService){}
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
@@ -16,8 +17,9 @@ export class HeroesComponent {
     this.getHeroes();
   }
   heroes: Hero[]=  [];
-  selectedHero?: Hero;
+/*   selectedHero?: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
-  }
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  } */
 }
